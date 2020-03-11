468af9829caebb1138ae70ede3bfae8fd8b74a75
Kevin Lueke <kevinlueke66@gmail.com>
2020-03-11 15:23:11 -0400

    A5 - 1 Security Misconfiguration

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var session = require("express-session");
 // var csrf = require('csurf');
 var consolidate = require("consolidate"); // Templating library adapter for Express
 var swig = require("swig");
-// var helmet = require("helmet");
+var helmet = require("helmet");
 var MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
 var http = require("http");
 var marked = require("marked");
 //var helmet = require("helmet");
-//var nosniff = require('dont-sniff-mimetype');
+var nosniff = require('dont-sniff-mimetype');
 var app = express(); // Web framework to handle routing requests
 var routes = require("./app/routes");
 var config = require("./config/config"); // Application config properties
@@ -37,20 +37,19 @@ MongoClient.connect(config.db, function(err, db) {
     }
     console.log("Connected to the database: " + config.db);
 
-    /*
     // Fix for A5 - Security MisConfig
     // TODO: Review the rest of helmet options, like "xssFilter"
     // Remove default x-powered-by response header
     app.disable("x-powered-by");
 
     // Prevent opening page in frame or iframe to protect from clickjacking
-    app.use(helmet.xframe());
+    app.use(helmet.frameguard());
 
     // Prevents browser from caching and storing page
     app.use(helmet.noCache());
 
     // Allow loading resources only from white-listed domains
-    app.use(helmet.csp());
+    app.use(helmet.contentSecurityPolicy());
 
     // Allow communication only on HTTPS
     app.use(helmet.hsts());
@@ -64,7 +63,6 @@ MongoClient.connect(config.db, function(err, db) {
 
 
     app.use(nosniff());
-    */
 
     // Adding/ remove HTTP Headers for security
     app.use(favicon(__dirname + "/app/assets/favicon.ico"));
@@ -78,28 +76,24 @@ MongoClient.connect(config.db, function(err, db) {
 
     // Enable session management using express middleware
     app.use(session({
-        // genid: function(req) {
-        //    return genuuid() // use UUIDs for session IDs
-        //},
+      //  genid: function(req) {
+      //      return genuuid(); // use UUIDs for session IDs
+      //  },
         secret: config.cookieSecret,
         // Both mandatory in Express v4
         saveUninitialized: true,
-        resave: true
-        /*
+        resave: true,
         // Fix for A5 - Security MisConfig
         // Use generic cookie name
         key: "sessionId",
-        */
 
         // Fix for A3 - XSS
         // TODO: Add "maxAge"
-      /*
        cookie: {
             httpOnly: true,
             // Remember to start an HTTPS server to get this working
             secure: true
          }
-         */
     }));
 
     /*
